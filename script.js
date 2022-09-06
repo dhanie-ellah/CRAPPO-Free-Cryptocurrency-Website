7eacbd08765d388d9d67f218fcf1b0cb8e29a76a
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-09-05 18:37:32 -0700

    update to onclick in card section

--- script.js
+++ script.js
@@ -16,10 +16,64 @@ closeBtn.onclick = () => {
 }
 
 // CHANGING CARD BACKGROUND AND COLOR ON CLICK
-// const cryptocurrency = document.querySelectorAll('.cryptocurrency')
-// const rightBtn = document.querySelectorAll('.right')
-// const mineBtn = document.querySelectorAll('.changebtn')
-// cryptocurrency.forEach(() => {
-//      rightBtn.addEventListener('click', () => {
-//      })
-// })
+const bitcoin = document.getElementById('bitcoin')
+const ethereum = document.getElementById('ethereum')
+const litecoin = document.getElementById('litecoin')
+const btcBtn = document.getElementById('btc-btn')
+const ethBtn = document.getElementById('eth-btn')
+const lteBtn = document.getElementById('lte-btn')
+const mineBtc = document.getElementById('mine-btc')
+const mineEth = document.getElementById('mine-eth')
+const mineLte = document.getElementById('mine-lte')
+
+btcBtn.onclick = () => {
+     // BITCOIN CARD 
+     bitcoin.style.backgroundColor = 'var(--secondary)'
+     bitcoin.style.color = 'white'
+     btcBtn.style.display = 'none'
+     mineBtc.style.display = 'block'
+     // ETHEREUM CARD 
+     ethereum.style.backgroundColor = 'transparent'
+     ethereum.style.color = 'black'
+     ethBtn.style.display = 'block'
+     mineEth.style.display = 'none'
+     // LITECOIN CARD 
+     litecoin.style.backgroundColor = 'transparent'
+     litecoin.style.color = 'black'
+     lteBtn.style.display = 'block'
+     mineLte.style.display = 'none'
+}
+ethBtn.onclick = () => {
+     // BITCOIN CARD 
+     bitcoin.style.backgroundColor = 'transparent'
+     bitcoin.style.color = 'black'
+     btcBtn.style.display = 'block'
+     mineBtc.style.display = 'none'
+     // ETHEREUM CARD 
+     ethereum.style.backgroundColor = 'var(--secondary)'
+     ethereum.style.color = 'white'
+     ethBtn.style.display = 'none'
+     mineEth.style.display = 'block'
+     // LITECOIN CARD 
+     litecoin.style.backgroundColor = 'transparent'
+     litecoin.style.color = 'black'
+     lteBtn.style.display = 'block'
+     mineLte.style.display = 'none'
+}
+lteBtn.onclick = () => {
+// BITCOIN CARD 
+     bitcoin.style.backgroundColor = 'transparent'
+     bitcoin.style.color = 'black'
+     btcBtn.style.display = 'block'
+     mineBtc.style.display = 'none'
+     // ETHEREUM CARD 
+     ethereum.style.backgroundColor = 'transparent'
+     ethereum.style.color = 'black'
+     ethBtn.style.display = 'block'
+     mineEth.style.display = 'none'
+     // LITECOIN CARD 
+     litecoin.style.backgroundColor = 'var(--secondary)'
+     litecoin.style.color = 'white'
+     lteBtn.style.display = 'none'
+     mineLte.style.display = 'block'
+}
